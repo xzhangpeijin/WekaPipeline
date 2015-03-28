e4fbe59636b17b18d79bb5be614cde73260cce9f
Peijin Zhang <xzhangpeijin@gmail.com>
2015-03-27 22:12:13 -0400

    Ranking code

--- src/main/java/org/bcl/project/mimic/RankCount.java
+++ src/main/java/org/bcl/project/mimic/RankCount.java
@@ -6,155 +6,145 @@ import java.util.*;
 
 public class RankCount
 {
+  private static final String RANK_FILE = "I:\\Documents\\PRIMES\\SingleLayerRankings.txt";
+  private static final int[] HOURS = {1,2,3,6,12,18,24,48,72,96};
+  private static final int NUM_CATEGORIES = 5;
+  
 	public RankCount() throws Exception
 	{
-		int[] hours = {1,2,3,6,12,18,24,48,72,96,120};
-		HashMap<Integer, Integer> hourmap = new HashMap<Integer, Integer>();
-		for(int x = 0; x < hours.length; x++)
-			hourmap.put(hours[x], x);
+		Map<Integer, Integer> hourmap = new HashMap<Integer, Integer>();
+		for(int x = 0; x < HOURS.length; x++)
+			hourmap.put(HOURS[x], x);
 		
-		ArrayList<String> attributes = new ArrayList<String>();
-		ArrayList<int[]> rankings = new ArrayList<int[]>();
-		BufferedReader in = new BufferedReader(new FileReader(new File("AttributeRanks.txt")));
+		Map<String, int[][]> rankings = new HashMap<String, int[][]>();
+		BufferedReader in = new BufferedReader(new FileReader(new File(RANK_FILE)));
 		String nextline = "";
-		int hour = Integer.MIN_VALUE;
 		while((nextline = in.readLine()) != null)
 		{
-			if(nextline.contains("temporal.extract.v14") && nextline.contains("hours.csv"))
+			if(nextline.contains("temporal.extract.14") && nextline.contains("none.discretized"))
 			{
-				String filename = nextline.substring(6);
-				hour = Integer.parseInt(filename.substring(27, filename.indexOf(".hours")));
+				String filename = nextline.substring("File: ".length());
+				int hour = Integer.parseInt(filename.substring(
+				    filename.indexOf("first.") + "first.".length(), filename.indexOf(".hours")));
+				int category = Integer.parseInt(filename.substring(
+				    filename.indexOf("category.") + "category.".length(), filename.indexOf(".csv")));
 				
 				int rank = 1;
-				nextline = in.readLine();
-				nextline = in.readLine();
+				// Skip two lines
+				in.readLine(); in.readLine();
 				while(!(nextline = in.readLine()).contains("Case"))
 				{
 					String attribute = nextline.split(": ")[0].trim();
-					if(attributes.contains(attribute))
-					{
-						int[] ranking = rankings.get(attributes.indexOf(attribute));
-						ranking[hourmap.get(hour)] = rank;
+					if (!rankings.containsKey(attribute)) {
+					  rankings.put(attribute, new int[NUM_CATEGORIES][HOURS.length]);
 					}
-					else
-					{
-						attributes.add(attribute);
-						int[] ranking = new int[hours.length];
-						for(int x = 0; x < hours.length; x++)
-							ranking[x] = Integer.MIN_VALUE;
-						ranking[hourmap.get(hour)] = rank;
-						rankings.add(ranking);
-					}
-					rank++;
+					rankings.get(attribute)[category - 1][hourmap.get(hour)] = rank++;
 				}
 			}
 		}
 		in.close();
 		
-		ArrayList<int[]> diffs = new ArrayList<int[]>();
-		for(int x = 0; x < attributes.size(); x++)
-		{
-			int[] ranking = rankings.get(x);
-			int[] diff = new int[ranking.length - 1];
-			int cur = ranking[0];
-			for(int y = 1; y < ranking.length; y++)
-			{
-				if(cur != Integer.MIN_VALUE && ranking[y] != Integer.MIN_VALUE)
-					diff[y - 1] = ranking[y] - cur;
-				else
-					diff[y - 1] = Integer.MIN_VALUE;
-				cur = ranking[y];
-			}
-			diffs.add(diff);
-		}
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(new File("AttributeRanks.csv"))));
+    StringBuffer title = new StringBuffer();
+    title.append("Attribute");
+    for (int x = 0; x < NUM_CATEGORIES; x++) {
+      for (int y = 0; y < HOURS.length; y++) {
+        title.append(String.format(",Category %d Hour %d", x + 1, HOURS[y]));
+      }
+    }
+    out.println(title.toString());
+    
+    for (String attribute : rankings.keySet()) {
+      StringBuffer line = new StringBuffer();
+      line.append(attribute);
+      int[][] rank = rankings.get(attribute);
+      for (int x = 0; x < NUM_CATEGORIES; x++) {
+        for (int y = 0; y < HOURS.length; y++) {
+          line.append(",");
+          if (rank[x][y] != 0) {
+            line.append(rank[x][y]);
+          }
+        }
+      }
+      out.println(line.toString());
+    }
+    out.flush();
+    out.close();
 		
-		ArrayList<double[]> stats = new ArrayList<double[]>();
-		for(int x = 0; x < attributes.size(); x++)
-		{
-			int[] diff = diffs.get(x);
-			int max = Integer.MIN_VALUE;
-			int min = Integer.MAX_VALUE;
-			int count = 0;
-			int tot = 0;
-			int squared = 0;
-			for(int y = 0; y < diff.length; y++)
-			{
-				if(diff[y] != Integer.MIN_VALUE)
-				{
-					tot += diff[y];
-					squared += diff[y] * diff[y];
-					count++;
-					max = Math.max(max, diff[y]);
-					min = Math.min(min, diff[y]);
-				}
-			}
-			int[] a = diff.clone();
-			Arrays.sort(a);
-			double[] stat = new double[7];
-			if(count != 0)
-			{
-				stat[0] = (double)tot / count;
-				stat[2] = (double)squared / count;
-			}
-			else
-			{
-				stat[0] = Integer.MIN_VALUE;
-				stat[2] = Integer.MIN_VALUE;
-			}
-			stat[1] = median(a);
-			
-			stat[3] = max;
-			stat[4] = min;
-			
-			tot = 0;
-			count = 0;
-			int[] rank = rankings.get(x);
-			for(int y = 0; y < rank.length; y++)
-			{
-				if(rank[y] != Integer.MIN_VALUE)
-				{
-					tot += rank[y];
-					count++;
-				}
-			}
-			a = rank.clone();
-			Arrays.sort(a);
-			stat[5] = (double)tot / count;
-			stat[6] = median(a);
-		
-//			System.out.print(attributes.get(x) + " ");
+//		ArrayList<int[]> diffs = new ArrayList<int[]>();
+//		for(int x = 0; x < attributes.size(); x++)
+//		{
+//			int[] ranking = rankings.get(x);
+//			int[] diff = new int[ranking.length - 1];
+//			int cur = ranking[0];
+//			for(int y = 1; y < ranking.length; y++)
+//			{
+//				if(cur != Integer.MIN_VALUE && ranking[y] != Integer.MIN_VALUE)
+//					diff[y - 1] = ranking[y] - cur;
+//				else
+//					diff[y - 1] = Integer.MIN_VALUE;
+//				cur = ranking[y];
+//			}
+//			diffs.add(diff);
+//		}
+//		
+//		ArrayList<double[]> stats = new ArrayList<double[]>();
+//		for(int x = 0; x < attributes.size(); x++)
+//		{
+//			int[] diff = diffs.get(x);
+//			int max = Integer.MIN_VALUE;
+//			int min = Integer.MAX_VALUE;
+//			int count = 0;
+//			int tot = 0;
+//			int squared = 0;
 //			for(int y = 0; y < diff.length; y++)
-//				System.out.print(diff[y] + " ");
-//			System.out.println();
+//			{
+//				if(diff[y] != Integer.MIN_VALUE)
+//				{
+//					tot += diff[y];
+//					squared += diff[y] * diff[y];
+//					count++;
+//					max = Math.max(max, diff[y]);
+//					min = Math.min(min, diff[y]);
+//				}
+//			}
+//			int[] a = diff.clone();
+//			Arrays.sort(a);
+//			double[] stat = new double[7];
+//			if(count != 0)
+//			{
+//				stat[0] = (double)tot / count;
+//				stat[2] = (double)squared / count;
+//			}
+//			else
+//			{
+//				stat[0] = Integer.MIN_VALUE;
+//				stat[2] = Integer.MIN_VALUE;
+//			}
+//			stat[1] = median(a);
+//			
+//			stat[3] = max;
+//			stat[4] = min;
+//			
+//			tot = 0;
+//			count = 0;
+//			int[] rank = rankings.get(x);
 //			for(int y = 0; y < rank.length; y++)
-//				System.out.print(rank[y] + " ");
-//			System.out.println();
-			stats.add(stat);
-		}
-		
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(new File("AttributeRanks.csv"))));
-		out.println("Attribute,Hour 1,Hour 2, Hour 3,Hour 6,Hour 12,Hour 18,"
-				+ "Hour 24,Hour 48,Hour 72,Hour 96,Hour 120,Average change,"
-				+ "Median change,Average squared change,Max change,Min change,Average rank,Median rank");
-		for(int x = 0; x < attributes.size(); x++)
-		{
-			int[] ranking = rankings.get(x);
-			double[] stat = stats.get(x);
-			out.print(attributes.get(x));
-			for(int y = 0; y < ranking.length; y++)
-				if(ranking[y] != Integer.MIN_VALUE)
-					out.print("," + ranking[y]);
-				else
-					out.print(",");
-			for(int y = 0; y < stat.length; y++)
-				if(stat[y] != Integer.MIN_VALUE && stat[y] != Integer.MAX_VALUE)
-					out.print("," + stat[y]);
-				else
-					out.print(",");
-			out.println();
-		}
-		out.flush();
-		out.close();
+//			{
+//				if(rank[y] != Integer.MIN_VALUE)
+//				{
+//					tot += rank[y];
+//					count++;
+//				}
+//			}
+//			a = rank.clone();
+//			Arrays.sort(a);
+//			stat[5] = (double)tot / count;
+//			stat[6] = median(a);
+//		
+//			stats.add(stat);
+//		}
+//		
 	}
 	
 	double median(int[] arr)
